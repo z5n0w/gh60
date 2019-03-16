fd24244e61a9b9c0a73712a321f08c4653fddbc8
z5n0w <836702701zs@gmail.com>
2019-03-16 13:18:10 +0800

    Update keymap.c
    fn key modify

--- keymap.c
+++ keymap.c
@@ -16,7 +16,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_GRAVE,KC_F1,   KC_F2,   KC_F3,   KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,   KC_F10,  KC_F11,   KC_F12,  KC_DEL,   KC_NO,    \
       KC_TRNS, KC_BTN1,KC_MS_U, KC_BTN2, KC_NO,KC_NO ,KC_NO ,KC_NO,  KC_WH_U, KC_NO,   KC_MEDIA_PLAY_PAUSE, KC_MEDIA_PREV_TRACK,  KC_MEDIA_NEXT_TRACK, KC_INSERT,    \
       KC_TRNS,  KC_MS_L, KC_MS_D, KC_MS_R,  KC_NO,  KC_NO,  KC_NO, KC_WH_L, KC_WH_D, KC_WH_L,  KC_VOLD, KC_VOLU,  KC_NO,  KC_MUTE,    \
-      KC_TRNS,  KC_NO,   KC_NO,   KC_NO,    BL_TOGG,   BL_DEC,   BL_INC,  KC_NO, KC_NO,    KC_PSCR, KC_SLCK, KC_BRK,  KC_PGUP,    KC_NO ,  KC_TRNS,   \
+      KC_TRNS,  KC_NO,   KC_NO,   KC_NO,    F(1),   F(2),   F(3),  KC_NO, KC_NO,    KC_PSCR, KC_SLCK, KC_BRK,  KC_PGUP,    KC_NO ,  KC_TRNS,   \
       KC_TRNS,  KC_TRNS, KC_TRNS,                          KC_NO,                          F(0),    KC_HOME,  KC_PGDOWN,  KC_END,  KC_MENU),
 
 };
@@ -24,9 +24,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Custom Actions
 const uint16_t PROGMEM fn_actions[] = {
     [0] = ACTION_LAYER_MOMENTARY(1),  // to Fn overlay
-};
-
-// Loop
-void matrix_scan_user(void) {
-  // Empty
+    [1] = backlight_toggle(), 
+    [2] = backlight_decrease(),
+    [3] = backlight_increase(),
 };
